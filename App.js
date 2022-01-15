371c07db3d4de06c39a696a76ff0311ba4d4f9c0
Atik <atik@Shakils-MacBook-Pro.local>
2022-01-15 11:19:59 +0600

    Initial Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>OMG! I am in ANDROID now</Text>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    color: '#000',
+  },
+})
---
+++ README.md
@@ -0,0 +1,1 @@
+# eyes-on-coin
