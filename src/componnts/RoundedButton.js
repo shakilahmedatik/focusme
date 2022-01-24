8d16c5e84e5b24090ead8098a755ffdc4d653b61
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-01-24 23:53:51 +0600

    Button Added

--- src/componnts/RoundedButton.js
+++ src/componnts/RoundedButton.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 
-const RoundedButton = () => {
-  return <div></div>
+export const RoundedButton = ({
+  style = {},
+  textSize = {},
+  size = 125,
+  ...props
+}) => {
+  return <TouchableOpacity style={[style]} />
 }
+
+const styles = style =>
+  StyleSheet.create({
+    radius: {
+      borderRadius: size / 2,
+      width: size,
+      height: size,
+      alignItems: 'center',
+    },
+  })
 
 export default RoundedButton
